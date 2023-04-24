2bb2b07533b16242d090f79792a03067911c6d75
juanitoserna <sernacastanojuan11gmail.com>
2023-04-24 11:58:44 -0500

    buscando cliente y vendedro

--- JFrame/Ejercicio_factura/Factura.java
+++ JFrame/Ejercicio_factura/Factura.java
@@ -1,11 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Factura extends JFrame{
     //atributos
+    private Persona arreglo_cliente[];
+    private Persona arreglo_vendedor[];
+    private JLabel titulo;
+    private JLabel etq_cedula;
+    private JTextField input_cedula;
+    private JButton buscar_01;
+    private JLabel etq_nombre;
+    private JTextField input_nombre;
+    private JLabel etq_direccion;
+    private JTextField input_direccion;
+    private JLabel titulo_02;
+    private JLabel etq_cedula_vendedor;
+    private JTextField input_cedula_vendedor;
+    private JButton buscar_02;
+    private JLabel etq_nombre_vender;
+    private JTextField input_nombre_vender;
+    private JLabel titulo_03;
+    private JLabel etq_ID;
+    private JLabel nombre_producto;
+    private JLabel cant;
+    private JTextField input_etq_ID;
+    private JTextField input_nombre_producto;
+    private JTextField input_cant;
+    private JButton agregar;
+    private JLabel etq_total;
 
-    public Factura(){
 
+
+    public Factura( Persona[] arreglo_cliente , Persona[] arreglo_vendedor){
+        this.arreglo_cliente = arreglo_cliente;
+        this.arreglo_vendedor =  arreglo_vendedor;
 
         initComponent();
     }
@@ -27,7 +57,7 @@ public class Factura extends JFrame{
         containerP.setOpaque(true);
         containerP.setBackground(Color.decode("#46F6E9"));
          
-        JLabel titulo = new JLabel("Datos Cliente: ");
+        titulo = new JLabel("Datos Cliente: ");
 		restriccion.gridy = 0;
 		restriccion.gridx = 0;
 		restriccion.gridheight = 1;
@@ -40,7 +70,7 @@ public class Factura extends JFrame{
 		containerP.add( titulo, restriccion );
 
         //---------- SEGUNDA COLUMNA ----------        
-        JLabel etq_cedula = new JLabel("CEDULA: ");
+        etq_cedula = new JLabel("CEDULA: ");
         restriccion.gridy = 1;
         restriccion.gridx = 0;
         restriccion.gridheight = 1;
@@ -51,7 +81,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(etq_cedula , restriccion);
         
-        JTextField input_cedula = new JTextField();
+        input_cedula = new JTextField();
         restriccion.gridy = 1;
         restriccion.gridx = 1;
         restriccion.gridheight = 1;
@@ -62,7 +92,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(input_cedula , restriccion);
 
-        JButton buscar_01 = new JButton("BUSCAR");
+        buscar_01 = new JButton("BUSCAR");
         restriccion.gridy = 1;
         restriccion.gridx = 3;
         restriccion.gridheight = 1;
@@ -77,7 +107,7 @@ public class Factura extends JFrame{
 
         //-------- TERCERA COLUMNA --------
 
-        JLabel etq_nombre =  new JLabel("NOMBRE: ");
+        etq_nombre =  new JLabel("NOMBRE: ");
         restriccion.gridy = 2;
         restriccion.gridx = 0;
         restriccion.gridheight = 1;
@@ -88,7 +118,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(etq_nombre , restriccion);      
 
-        JTextField input_nombre = new JTextField();
+        input_nombre = new JTextField();
         restriccion.gridy = 2;
         restriccion.gridx = 1;
         restriccion.gridheight = 1;
@@ -100,7 +130,7 @@ public class Factura extends JFrame{
         containerP.add(input_nombre , restriccion);
 
         //---------- CUARTA COLUMNA ---------
-        JLabel etq_direccion =  new JLabel("DIRECCION: ");
+        etq_direccion =  new JLabel("DIRECCION: ");
         restriccion.gridy = 3;
         restriccion.gridx = 0;
         restriccion.gridheight = 1;
@@ -111,7 +141,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(etq_direccion , restriccion);      
 
-        JTextField input_direccion = new JTextField();
+        input_direccion = new JTextField();
         restriccion.gridy = 3;
         restriccion.gridx = 1;
         restriccion.gridheight = 1;
@@ -123,7 +153,7 @@ public class Factura extends JFrame{
         containerP.add(input_direccion , restriccion);
 
         //------- QUINTA COLUMNA ------
-        JLabel titulo_02 = new JLabel("DATOS VENDEDOR: ");
+        titulo_02 = new JLabel("DATOS VENDEDOR: ");
 		restriccion.gridy = 4;
 		restriccion.gridx = 0;
 		restriccion.gridheight = 1;
@@ -136,7 +166,7 @@ public class Factura extends JFrame{
 		containerP.add( titulo_02, restriccion );
 
         //---------- SEXTA  COLUMNA ----------        
-        JLabel etq_cedula_vendedor = new JLabel("CEDULAS: ");
+        etq_cedula_vendedor = new JLabel("CEDULAS: ");
         restriccion.gridy = 5;
         restriccion.gridx = 0;
         restriccion.gridheight = 1;
@@ -147,7 +177,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(etq_cedula_vendedor , restriccion);
         
-        JTextField input_cedula_vendedor = new JTextField();
+        input_cedula_vendedor = new JTextField();
         restriccion.gridy = 5;
         restriccion.gridx = 1;
         restriccion.gridheight = 1;
@@ -158,7 +188,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(input_cedula_vendedor , restriccion);
 
-        JButton buscar_02 = new JButton("BUSCAR");
+        buscar_02 = new JButton("BUSCAR");
         restriccion.gridy = 5;
         restriccion.gridx = 3;
         restriccion.gridheight = 1;
@@ -173,7 +203,7 @@ public class Factura extends JFrame{
 
         //-------- SEPTIMA COLUMNA --------
 
-        JLabel etq_nombre_vender =  new JLabel("NOMBRE: ");
+        etq_nombre_vender =  new JLabel("NOMBRE: ");
         restriccion.gridy = 6;
         restriccion.gridx = 0;
         restriccion.gridheight = 1;
@@ -184,7 +214,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(etq_nombre_vender , restriccion);      
 
-        JTextField input_nombre_vender = new JTextField();
+        input_nombre_vender = new JTextField();
         restriccion.gridy = 6;
         restriccion.gridx = 1;
         restriccion.gridheight = 1;
@@ -196,7 +226,7 @@ public class Factura extends JFrame{
         containerP.add(input_nombre_vender , restriccion);
 
         //------- OCTAVA COLUMNA ------
-        JLabel titulo_03 = new JLabel("PRODUCTOS: ");
+        titulo_03 = new JLabel("PRODUCTOS: ");
 		restriccion.gridy = 7;
 		restriccion.gridx = 0;
 		restriccion.gridheight = 1;
@@ -209,7 +239,7 @@ public class Factura extends JFrame{
 		containerP.add( titulo_03, restriccion );
 
         //-------- NOVENA COLUMNA ---------
-        JLabel etq_ID = new JLabel("ID");
+        etq_ID = new JLabel("ID");
         etq_ID.setHorizontalAlignment( JLabel.CENTER );
         restriccion.gridy = 8;
         restriccion.gridx = 0;
@@ -221,7 +251,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(etq_ID , restriccion);
         
-        JLabel nombre_producto = new JLabel("NOMBRE");
+        nombre_producto = new JLabel("NOMBRE");
         nombre_producto.setHorizontalAlignment( JLabel.CENTER );
         restriccion.gridy = 8;
         restriccion.gridx = 1;
@@ -233,7 +263,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(nombre_producto , restriccion);
 
-        JLabel cant = new JLabel("CANT");
+        cant = new JLabel("CANT");
         restriccion.gridy = 8;
         restriccion.gridx = 2;
         restriccion.gridheight = 1;
@@ -248,7 +278,7 @@ public class Factura extends JFrame{
 
 
 
-        JTextField input_etq_ID = new JTextField();
+        input_etq_ID = new JTextField();
         restriccion.gridy = 9;
         restriccion.gridx = 0;
         restriccion.gridheight = 1;
@@ -259,7 +289,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(input_etq_ID , restriccion);
         
-        JTextField input_nombre_producto = new JTextField();
+        input_nombre_producto = new JTextField();
         restriccion.gridy = 9;
         restriccion.gridx = 1;
         restriccion.gridheight = 1;
@@ -270,7 +300,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(input_nombre_producto , restriccion);
 
-        JTextField input_cant = new JTextField();
+        input_cant = new JTextField();
         restriccion.gridy = 9;
         restriccion.gridx = 2;
         restriccion.gridheight = 1;
@@ -281,7 +311,7 @@ public class Factura extends JFrame{
         restriccion.insets = new Insets(0, 0, 5, 0);
         containerP.add(input_cant ,restriccion);
 
-        JButton agregar = new JButton("AGREGAR");
+        agregar = new JButton("AGREGAR");
         restriccion.gridy = 9;
         restriccion.gridx = 3;
         restriccion.gridheight = 1;
@@ -302,7 +332,7 @@ public class Factura extends JFrame{
         
 
 
-        JLabel etq_total = new JLabel("----");
+        etq_total = new JLabel("----");
         etq_total.setOpaque(true);
         etq_total.setBackground(Color.WHITE);
         restriccion.gridy = 10;
@@ -320,5 +350,74 @@ public class Factura extends JFrame{
 		setVisible(true);
 		revalidate();
 		repaint();
+
+        ActionListener evento_01 = new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                buscarCliente();
+            } 
+        };
+        buscar_01.addActionListener(evento_01);
+        
+        ActionListener evento_buscar_vendedor = new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                buscarVendedor();
+            }
+        };
+        buscar_02.addActionListener(evento_buscar_vendedor);
     }
+
+    public void buscarCliente(){
+        String texto = input_cedula.getText();
+		boolean encontrado = false;
+		for (int i=0; i<this.arreglo_cliente.length; i++) {
+			if(this.arreglo_cliente[i]!=null && this.arreglo_cliente[i].getCedula().equalsIgnoreCase(texto)){
+				this.input_nombre.setText( this.arreglo_cliente[i].getNombre() );
+				this.input_direccion.setText( this.arreglo_cliente[i].getDireccion() );
+				encontrado = true;
+				break;
+			}
+		}
+
+        if(encontrado){
+			deshabilitarInput(this.input_nombre);
+			deshabilitarInput(this.input_direccion);
+			this.input_cedula_vendedor.requestFocus();
+		}else{
+			habilitarInput(this.input_nombre);
+			habilitarInput(this.input_direccion);
+			this.input_nombre.requestFocus();
+		}
+    }
+
+    public void buscarVendedor(){
+        String texto = input_cedula_vendedor.getText();
+        boolean encontrado = false;
+        for (int i = 0; i < arreglo_vendedor.length; i++) {
+            if (this.arreglo_vendedor[i]!= null && this.arreglo_vendedor[i].getCedula().equalsIgnoreCase(texto)) {
+                this.input_nombre_vender.setText( this.arreglo_vendedor[i].getNombre());
+                encontrado = true;
+                break;
+            }
+        }
+
+        if(encontrado){
+			deshabilitarInput(this.input_nombre_vender);
+			this.input_cedula.requestFocus();
+		}else{
+			habilitarInput(this.input_nombre_vender);
+			this.input_nombre.requestFocus();
+		}
+    }
+
+    public void deshabilitarInput(JTextField input){
+		input.setEditable(false);
+		input.setEnabled(false);
+		input.setDisabledTextColor(Color.black);
+	}
+
+    public void habilitarInput(JTextField input){
+		input.setText("");
+		input.setEditable(true);
+		input.setEnabled(true);
+	}
 }
--- JFrame/Ejercicio_factura/Principal.java
+++ JFrame/Ejercicio_factura/Principal.java
@@ -2,6 +2,14 @@
 public class Principal{
     public static void main (String[]args){
 
-        Factura FC_01 = new Factura();
+        
+        Persona arreglo_cliente[] = new Persona [100];
+        Persona arreglo_vendedor[] = new Persona [100];
+
+        arreglo_cliente[0] = new Persona("1059784369", "Juan Esteban", "clle 20");
+        
+        arreglo_vendedor[0] = new Persona("1059784368", "Vendedor 1", " ");
+
+        Factura constructor = new Factura(arreglo_cliente, arreglo_vendedor);
     }
 }
